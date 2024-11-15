8b9ba0ba5e3bb36733b396c880cbf9e83d3fc2ad
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-15 15:39:06 -0300

    feat: criar página de login com componente Login integrado

---
+++ src/pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="login-container">
+        <Login />
+      
+    </div>
+</template>
+
+<script>
+import Login from '@/components/Login.vue';
+
+
+export default {
+    components: {
+        Login
+    },
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para a página de login */
+</style>

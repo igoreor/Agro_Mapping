fadd1f4602adbb9a3d8d8af185422f2ba2b3c663
igoreor <igoreor24@gmail.com>
2024-11-15 00:05:12 -0300

    feat(pages): criar o aquivo pages

---
+++ src/pages/home.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <MainNav />
+        <MainHeader />
+        <SobreSection />
+        <button class="advertise-button" @click="goToVendas">Anuncie seus produtos aqui</button>
+    </div>
+</template>
+
+<script>
+import MainNav from '@/components/MainNav.vue';
+import MainHeader from '@/components/MainHeader.vue';
+import SobreSection from '@/components/SobreSection.vue';
+
+
+export default {
+    components: {
+        MainNav,
+        MainHeader,
+        SobreSection,
+    },
+    methods: {
+        goToVendas() {
+            this.$router.push('/vendas');
+        },
+    },
+};
+</script>
+
+<style>
+/* Estilos específicos para Home se necessário */
+</style>

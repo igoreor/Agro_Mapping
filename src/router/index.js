f3c66099398d181ccf4010658447aefec4d465ee
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-15 15:41:05 -0300

    feat: adicionar rotas para as páginas Login e Registro

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import home from "../pages/home.vue";
+import Login from "../pages/login.vue";
+import Registro from "../pages/registro.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +12,17 @@ const router = createRouter({
       components: {
         default: home,
       },
+      
+    },
+    {
+      path: '/registro',
+      name: 'Registro',
+      component: Registro,
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login,
     },
   ],
 });

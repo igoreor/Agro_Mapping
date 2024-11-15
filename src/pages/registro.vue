e93aa38d41fdd7a4b3d4b778a2750776125e9729
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-15 15:39:35 -0300

    feat: criar página de registro com componente Registro integrado

---
+++ src/pages/registro.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="register-container">
+        <Registro />
+    </div>
+</template>
+
+<script>
+import Registro from '@/components/Registro.vue';
+
+export default {
+    components: {
+
+        Registro
+    },
+};
+</script>
+
+<style scoped>
+/* Estilos específicos para a página de registro */
+</style>

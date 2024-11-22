496ee492085c700fcaa9de0545750edbbcd9e152
NathanDalbert <dalbert.nathan@gmail.com>
2024-11-21 21:10:11 -0300

    feat: implementar cadastro de contato

---
+++ src/pages/cadastrocontato.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="cadastro-contato-page">
+      <CadastroContato />
+    </div>
+  </template>
+  
+  <script>
+  import CadastroContato from "@/components/CadastroContato.vue";
+  
+  export default {
+    name: "CadastroContato",
+    components: {
+      CadastroContato,
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .cadastro-contato-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: #f5f5f5;
+  }
+  </style>
+  

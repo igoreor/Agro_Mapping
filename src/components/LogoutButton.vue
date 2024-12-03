26179cd088004ab7a62bf7e13c2327101ea18fd0
igoreor <igoreor24@gmail.com>
2024-12-03 12:09:07 -0300

    update(LogoutButton): deixar o botão sempre visível

--- src/components/LogoutButton.vue
+++ src/components/LogoutButton.vue
@@ -2,25 +2,20 @@
     <button class="logout-button" @click="logout">Logout</button>
 </template>
 
-
 <script>
 export default {
     methods: {
         logout() {
-            // Limpa o token do localStorage
             localStorage.removeItem('token');
             console.log('Token removido. Redirecionando para login...');
-            // Redireciona para a página de login
             this.$router.push('/login');
         },
     },
 };
 </script>
 
-
 <style>
 .logout-button {
-    /* Estilo opcional para o botão */
     background-color: #f44336;
     color: white;
     border: none;
@@ -28,13 +23,13 @@ export default {
     cursor: pointer;
     border-radius: 5px;
     font-size: 16px;
+    position: fixed;
+    top: 93%; 
+    right: 94%; 
+    z-index: 1000; 
 }
-
 
 .logout-button:hover {
     background-color: #d32f2f;
 }
 </style>
-
-
-

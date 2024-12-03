b570ad25c357c2a47c7d2f1baf3154ebb6f9aa69
Mickael-7 <mickael.albuquerque@upe.br>
2024-12-02 22:40:29 -0300

    feat(CadastroProdutos): criar page CadastroProdutos

---
+++ src/pages/CadastroProdutos.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+        <CadastroProdutos/>
+        
+    </div>
+</template>
+<script>
+import CadastroProdutos from '@/components/CadastroProdutos.vue';
+
+export default{
+    components:{
+        CadastroProdutos
+    }
+}
+</script>

1335ccbe5b0577dd0d9e96c8914db2a593daa9dc
Mickael-7 <mickael.albuquerque@upe.br>
2024-12-03 22:27:43 -0300

    feat(MeuPerfil): criar page de MeuPerfil

---
+++ src/pages/MeuPerfil.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <MainNav/>
+        <MeuPerfil/>
+        
+    </div>
+</template>
+<script>
+import MeuPerfil from '@/components/MeuPerfil.vue';
+import MainNav from '@/components/MainNav.vue';
+
+export default{
+    components:{
+        MeuPerfil,
+        MainNav
+    }
+}
+</script>
+<style scoped>
+/* Estilização local do componente */
+div {
+  padding: 20px;
+}
+</style>

b7637163116d25be79f9163eb8bd7269f34ab83b
Mickael-7 <mickael.albuquerque@upe.br>
2024-12-03 22:28:21 -0300

    feat(MeusAnuncios): criar page de Meus Anuncios

---
+++ src/pages/MeusAnuncios.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <MainNav/>
+        <MeusAnuncios/>
+        
+    </div>
+</template>
+<script>
+import MeusAnuncios from '@/components/MeusAnuncios.vue';
+import MainNav from '@/components/MainNav.vue';
+
+export default{
+    components:{
+        MeusAnuncios,
+        MainNav
+    }
+}
+</script>
+<style scoped>
+/* Estilização local do componente */
+div {
+  padding: 20px;
+}
+</style>

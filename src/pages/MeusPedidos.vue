093bb798a240a073f237d33cb3d7a1e3d2013e2b
Mickael-7 <mickael.albuquerque@upe.br>
2024-12-04 18:17:29 -0300

    feat(MeusPedidos): criar page MeusPedidos

---
+++ src/pages/MeusPedidos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+        <MainNav/>
+        <Meuspedidos/>
+        
+    </div>
+</template>
+<script>
+import Meuspedidos from '@/components/Meuspedidos.vue';
+import MainNav from '@/components/MainNav.vue';
+
+export default{
+    components:{
+        Meuspedidos,
+        MainNav
+    }
+}
+</script>
+<style scoped>
+/* Estilização local do componente */
+div {
+  padding: 20px;
+}
+</style>
